f5e5c040cdb08b30f8c6e6af126ebcad11364a64
Angel Ruiz <angel@comalatech.com>
2021-02-10 11:04:00 +0100

    Added two tests that fail and they should not

---
+++ src/test/helper/confluenceHelper.js
@@ -0,0 +1,26 @@
+
+
+const createPageUnderSpaceHomepage = (title, spaceKey) => _createContent(title, spaceKey, {ancestors: null})
+
+const createPageUnderSpaceRoot = (title, spaceKey) => _createContent(title, spaceKey)
+
+const _createContent = (title, spaceKey, {ancestors = []} = []) =>
+    confluence.createContent(
+        {
+            title,
+            type: 'page',
+            space: {
+                key: spaceKey,
+            },
+            body: {
+                storage: {
+                    value: 'This is a test...',
+                    representation: 'storage',
+                },
+            },
+            ancestors
+        },
+        {expand: ['ancestors']}
+    )
+
+module.exports = { createPageUnderSpaceHomepage, createPageUnderSpaceRoot }
--- src/test/integration/002.pageMovedWebhookBug.test.js
+++ src/test/integration/002.pageMovedWebhookBug.test.js
@@ -1,4 +1,6 @@
-describe('Initial checks', () => {
+const { createPageUnderSpaceRoot, createPageUnderSpaceHomepage } = require('../helper/confluenceHelper');
+
+describe('Step to reproduce bug in moving content REST endpoint', () => {
   beforeAll(async () => {
     await createSpaces();
   });
@@ -7,14 +9,39 @@ describe('Initial checks', () => {
     await removeSpaces();
   });
 
-  test('page_moved webhook should be triggered when using the moving content REST endpoint', async () => {
-    // Given: a page A
-
-    // and: a page B
+  test('When moving a page that sits under space root as child of a page that also seats under space root,' +
+      ' the page_moved webhook should be triggered to add a content property flag', async () => {
+    // Given: a page A located at Space Root === (ancestors = [])
+    const pageA = await createPageUnderSpaceRoot('A', testSpaceKey);
+    // and: a page B on located at Space Root === (ancestors = [])
+    const pageB = await createPageUnderSpaceRoot('B', testSpaceKey);
 
     // When: we move B as child of A
+    confluence.moveContent(pageB.id, 'append', pageA.id)
+    // and wait 5 seconds for the webhook to execute. We are aware this is not 100% reliable but it will do most of the time
+    await sleep(5000);
 
     // Then: the page_moved webhook is triggered and a content property flag is set
+    const flag = await confluence.getContentProperty(pageB.id, 'my-sample-app-flag');
+    expect(flag).not.toBeUndefined();
+    expect(flag.value).toBeTruthy();
+  });
 
+  test('When moving a page that sits under space root as child of a page tha sits under space homepage,' +
+      ' the page_moved webhook should be triggered to add a content property flag', async () => {
+    // Given: a page A located at Space Root === (ancestors = [])
+    const pageA = await createPageUnderSpaceHomepage('A', testSpaceKey);
+    // and: a page B on located at Space Root === (ancestors = [])
+    const pageB = await createPageUnderSpaceRoot('B', testSpaceKey);
+
+    // When: we move B as child of A
+    confluence.moveContent(pageB.id, 'append', pageA.id)
+    // and wait 5 seconds for the webhook to execute. We are aware this is not 100% reliable but it will do most of the time
+    await sleep(5000);
+
+    // Then: the page_moved webhook is triggered and a content property flag is set
+    const flag = await confluence.getContentProperty(pageB.id, 'my-sample-app-flag');
+    expect(flag).not.toBeUndefined();
+    expect(flag.value).toBeTruthy();
   });
 });
